579962d5068c841bcd1455e1435a81cf473c54ea
CristinaBulnes <cristinabulnesmaya@gmail.com>
2021-05-11 10:26:58 +0200

    refactor: create playlist view header and integrate to main view and edit view

--- packages/web/src/components/PlaylistViewHeader/index.jsx
+++ packages/web/src/components/PlaylistViewHeader/index.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 // import { startplaylist } from '../../redux/audioPlayer/audioPlayer-actions';
 
 import {
     addLikeToPlaylist,
     followPlaylist,
+    editPlaylist,
+    getPlaylist,
 } from '../../redux/Playlists/playlists-actions';
+
 import {
     useQuickMenu,
     useQuickMenuListener,
@@ -16,13 +20,23 @@ import LikeIcon from '../LikeButton';
 import './styles.scss';
 import PlaylistViewHeaderStyle from './styled';
 
-const PlaylistViewHeader = ({ playlist }) => {
+const PlaylistViewHeader = ({ playlist, from }) => {
     const dispatch = useDispatch();
     const [openMenu] = useQuickMenu();
+    const [title, setTitle] = useState(playlist.title);
+    const [type, setType] = useState(playlist.type);
+    const [publicAccess, setPublicAccess] = useState(playlist.publicAccess);
+    const [description, setDescription] = useState('');
+
     const { currentlyPlaying } = useSelector(store => store.audioPlayer);
     const play_pause = document.getElementsByClassName(
         'rhap_play-pause-button',
     );
+
+    useEffect(() => {
+        dispatch(getPlaylist(playlist._id));
+    }, [dispatch, playlist]);
+
     function reproduceplaylist() {
         if (playlist._id === currentlyPlaying.playlistId) {
             const simulateClick = new MouseEvent('click', {
@@ -53,22 +67,35 @@ const PlaylistViewHeader = ({ playlist }) => {
                     <input
                         type="text"
                         className="mega-playlist__info__type"
-                        value={playlist.type}
-                        readOnly
+                        readOnly={from === 'mainView'}
+                        value={type === 'Playlist' ? 'Playlist' : 'Album'}
+                        onClick={() => {
+                            if (from === 'editableView') {
+                                setType(
+                                    type === 'Playlist' ? 'Album' : 'Playlist',
+                                );
+                            }
+                        }}
                     />
                     <span> · </span>
                     <input
                         type="text"
                         className="mega-playlist__info__access"
-                        value={playlist.publicAccess ? 'Public' : 'Private'}
-                        readOnly
+                        value={publicAccess ? 'Public' : 'Private'}
+                        readOnly={from === 'mainView'}
+                        onClick={() => {
+                            if (from === 'editableView') {
+                                setPublicAccess(!publicAccess);
+                            }
+                        }}
                     />
                 </div>
                 <input
                     type="text"
                     className="mega-playlist__info__title"
-                    value={playlist.title}
-                    readOnly
+                    value={title}
+                    readOnly={from === 'mainView'}
+                    onChange={e => setTitle(e.target.value)}
                 />
                 <input
                     type="text"
@@ -80,8 +107,9 @@ const PlaylistViewHeader = ({ playlist }) => {
                 <input
                     type="text"
                     className="mega-playlist__info__description"
-                    value="playlist.description"
-                    readOnly
+                    value={description}
+                    readOnly={from === 'mainView'}
+                    onChange={e => setDescription(e.target.value)}
                 />
                 <div className="mega-playlist__info__container-info">
                     <p className="mega-playlist__info__container-info__likes">
@@ -99,21 +127,46 @@ const PlaylistViewHeader = ({ playlist }) => {
                     type="button"
                     onMouseDown={e => openMenu(e, playlist._id)}
                 />
-                <div className="mega-playlist__card__buttons">
-                    <LikeIcon
-                        zoom="true"
-                        handleLike={() =>
-                            dispatch(addLikeToPlaylist(playlist._id))
-                        }
-                        likes={playlist.likedBy}
-                    />
-                    <Button
-                        type="button"
-                        onClick={() => dispatch(followPlaylist(playlist._id))}
-                    >
-                        Follow
-                    </Button>
-                </div>
+                {from === 'mainView' && (
+                    <div className="mega-playlist__card__buttons">
+                        <LikeIcon
+                            zoom="true"
+                            handleLike={() =>
+                                dispatch(addLikeToPlaylist(playlist._id))
+                            }
+                            likes={playlist.likedBy}
+                        />
+                        <Button
+                            type="button"
+                            onClick={() =>
+                                dispatch(followPlaylist(playlist._id))
+                            }
+                        >
+                            Follow
+                        </Button>
+                    </div>
+                )}
+                {from === 'editableView' && (
+                    <div className="mega-playlist__card__buttons">
+                        <Link to={`/playlist/${playlist._id}`}>
+                            <Button
+                                type="button"
+                                onClick={() => {
+                                    dispatch(
+                                        editPlaylist(playlist._id, {
+                                            title,
+                                            description,
+                                            publicAccess,
+                                            type,
+                                        }),
+                                    );
+                                }}
+                            >
+                                Save
+                            </Button>
+                        </Link>
+                    </div>
+                )}
             </div>
         </PlaylistViewHeaderStyle>
     );
--- packages/web/src/pages/PlaylistView/Playlist.js
+++ packages/web/src/pages/PlaylistView/Playlist.js
@@ -3,9 +3,6 @@ import { Redirect, useParams } from 'react-router';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-import Button from '../../styles/components/Button/GenericButton';
-import LikeIcon from '../../components/LikeButton';
-
 import {
     addLikeToPlaylist,
     followPlaylist,
@@ -41,7 +38,7 @@ function Playlist() {
     }
     return (
         <>
-            <PlaylistViewHeader playlist={playlist} from="mainView"/>
+            <PlaylistViewHeader playlist={playlist} from="mainView" />
             <PlaylistViewStyle className="PlaylistView" image={playlist.img}>
                 <SongsList
                     songsToList={playlist.songs}
@@ -53,104 +50,3 @@ function Playlist() {
 }
 
 export default Playlist;
-
-/* <div className="PlaylistView__image">
-                    <img
-                        src={
-                            playlist.img ||
-                            'https://picsum.photos/seed/picsum/500'
-                        }
-                        alt="headerImg"
-                        width="100%"
-                    />
-                <Input
-                    type="text"
-                    name="name"
-                    id="name"
-                    value={name}
-                    className="disable-input"
-                    spellCheck="false"
-                    onChange={e => setName(e.target.value)}
-                />
-                        </div>
-                        <div>
-                            <div>
-                                <div>
-                                    <div className="song-modal__likes">
-                                        <p>{songs[songID].likes.length}</p>
-                                        <FontAwesomeIcon
-                                            icon={faHeart}
-                                            className={
-                                                songs[songID].likes
-                                                    ? 'text-indigo-800'
-                                                    : 'text-gray-400'
-                                            }
-                                            onClick={() =>
-                                                dispatch(addLikeToSong(songID))
-                                            }
-                                        />
-                                    </div>
-                                    <label htmlFor="author" className="">
-                                        Author
-                                    </label>
-                                    <Input
-                                        type="text"
-                                        name="author"
-                                        id="author"
-                                        value={author}
-                                        spellCheck="false"
-                                        onChange={e =>
-                                            setAuthor(e.target.value)
-                                        }
-                                    />
-                                </div>
-                                <div>
-                                    <label htmlFor="genre">Genre</label>
-                                    <Input
-                                        type="text"
-                                        name="genre"
-                                        id="genre"
-                                        value={genre}
-                                        spellCheck="false"
-                                        onChange={e => setGenre(e.target.value)}
-                                    />
-                                </div>
-                                <div>
-                                    <label htmlFor="uploader">Upload by</label>
-                                    <Input
-                                        type="text"
-                                        name="uploader"
-                                        id="uploader"
-                                        value={uploader}
-                                        spellCheck="false"
-                                        onChange={e =>
-                                            setUploader(e.target.value)
-                                        }
-                                    />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="song-modal__buttons">
-                        <Button
-                            type="button"
-                            onClick={() =>
-                                dispatch(
-                                    editSongByID(songID, {
-                                        name,
-                                        uploader,
-                                        author,
-                                        genre,
-                                    }),
-                                )
-                            }
-                        >
-                            Save changes
-                        </Button>
-                        <Button
-                            type="button"
-                            width="100px"
-                            onClick={() => dispatch(deleteSongByID(songID))}
-                        >
-                            Delete
-                        </Button> */
